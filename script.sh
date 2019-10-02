ee5085852b3430d41cf60565e65df10b2a343788
Christopher Sexton <csexton@users.noreply.github.com>
2019-10-02 17:27:39 -0400

    Add timeout
    Don't want this action running forever if it is being ignored. Might be
    nice to add a way to prevent the timeout from occurring if the user is
    currently logged in.

--- script.sh
+++ script.sh
@@ -23,8 +23,10 @@ tmate -S /tmp/tmate.sock wait tmate-ready
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 
-# Wait for connection to close
+# Wait for connection to close or timeout in 15 min
+timeout=$((15*60))
 while [ -S /tmp/tmate.sock ]; do
   sleep 1
+  timeout=$(($timeout-1))
+  [ $timeout -gt 0 ] || exit 1
 done
-
