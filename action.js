7d5cde8c3ee7494c6b0a522e81234d9935849583
Christopher Sexton <csexton@users.noreply.github.com>
2019-10-02 17:09:08 -0400

    Prune non-prod modules

---
+++ action.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+
+function run() {
+  try {
+    // This is just a thin wrapper around bash
+    const script = require('path').resolve(__dirname, 'script.sh');
+
+    console.log(script);
+    var child = require('child_process').execFile(script);
+    child.stdout.on('data', function(data) {
+      console.log(data.toString());
+    });
+
+    child.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      process.exit(code);
+    });
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
--- script.sh
+++ script.sh
@@ -2,29 +2,29 @@
 
 set -e
 
+# Install tmate on macOS or Ubuntu
 echo Setting up tmate...
-
 if [ -x "$(command -v brew)" ]; then
   brew install tmate
 fi
-
 if [ -x "$(command -v apt-get)" ]; then
   sudo apt-get install -y tmate openssh-client
 fi
 
+# Generate ssh key if needed
 [ -e ~/.ssh/id_rsa ] || ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
 
-
+# Run deamonized tmate
 echo Running tmate...
-
 tmate -S /tmp/tmate.sock new-session -d
 tmate -S /tmp/tmate.sock wait tmate-ready
 
+# Print connection info
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 
+# Wait for connection to close
 while [ -S /tmp/tmate.sock ]; do
   sleep 1
 done
-
 
